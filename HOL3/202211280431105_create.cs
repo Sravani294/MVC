025149b507aaf42455033905ae52abd6db7a30e8
Sravani294 <86480700+Sravani294@users.noreply.github.com>
2022-11-30 09:48:17 +0000

    HOL3

---
+++ HOL3/202211280431105_create.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.Entity.Migrations;
+namespace _HOL3.Migrations
+
+
+
+{
+
+
+    public partial class create : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+            "dbo.Users",
+
+            c => new
+            {
+
+                UserName = c.String(nullable: false, maxLength: 128),
+                Password = c.String(),
+            })
+                    .PrimaryKey(t => t.UserName);
+
+        }
+
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
+
+            
